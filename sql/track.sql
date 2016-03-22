07c323583c22ad1631774f4993e24d9bc15d6497
lamzin <olehtomas@gmail.com>
2016-03-23 00:33:06 +0200

    fix create statement

--- sql/track.sql
+++ sql/track.sql
@@ -1,11 +1,13 @@
-CREATE TABLE `orpheus`.`track` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `processing_stage` ENUM('ps_new', 'ps_processing', 'ps_done', 'ps_not_found') NOT NULL DEFAULT 'ps_new' COMMENT '',
-  `name` VARCHAR(128) NULL COMMENT '',
-  `author` VARCHAR(64) NULL COMMENT '',
-  `url_page` VARCHAR(256) NULL COMMENT '',
-  `url_track` VARCHAR(256) NULL COMMENT '',
-  `url_disk` VARCHAR(512) NULL COMMENT '',
-  `data` VARCHAR(4096) NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '');
+CREATE TABLE `track` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `processing_stage` enum('ps_new','ps_processing','ps_done','ps_not_found') NOT NULL DEFAULT 'ps_new',
+  `name` varchar(128) DEFAULT NULL,
+  `author` varchar(64) DEFAULT NULL,
+  `url_page` varchar(256) DEFAULT NULL,
+  `url_track` varchar(256) DEFAULT NULL,
+  `url_disk` varchar(512) DEFAULT NULL,
+  `site_id` int(11) DEFAULT '1',
+  `data` varchar(4096) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=236273 DEFAULT CHARSET=utf8;
